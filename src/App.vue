7803530b0b496c6178f173d82d6c7082c8ea167b
Houdaifa Amzil <35547273+Houdaifi@users.noreply.github.com>
2021-07-11 01:52:37 +0000

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+     <div class="container mx-auto px-12 p-4 pt-6">
+        <nav class="flex justify-between items-center text-gray-700 text-lg">
+          <div>
+            <h1 style="font-family: 'Fuggles', cursive;" class="text-6xl tracking-wider">Amzil Houdaifa</h1>
+          </div>
+          <div>
+            <router-link class="pr-8" to="/">Home</router-link>
+            <router-link class="pr-8" to="/projects">Projects</router-link>
+            <router-link class="pr-8" to="/cv">Resume</router-link>
+            <button class="rounded bg-blue-500 hover:bg-blue-600 px-4 py-2 text-white" to="/contact">Contact</button>
+          </div>
+        </nav>
+        <hr class="border-gray-300 mt-6 pb-28">
+        <router-view/>
+    </div>
+   
+  </div>
+</template>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Fuggles&display=swap');
+</style> 
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+    <div class="flex flex-row justify-between items-center">
+      <div class="flex flex-col justify-around space-y-4">
+        <div><h2 class="text-2xl">Hi! I'am <span class="text-blue-500 tracking-normal">Amzil Houdaifa</span> and</h2></div>
+        <div>
+          <h6 class="text-3xl text-justify tracking-tight text-gray-500 leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus est sed eaque distinctio saepe, commodi, nisi enim aliquam quae labore quidem vitae tempore! Fuga.</h6>
+        </div>
+      </div>
+       <div>
+        <img class="w-2/3" src="../assets/me.jpg" alt="Me">
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
