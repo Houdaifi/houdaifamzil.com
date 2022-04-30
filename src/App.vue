1d6c8d585ae2892323830e44cd8b240a3a2bd3bc
Houdaifi <houdaifaamzil@gmail.com>
2022-04-30 00:50:53 +0000

    Change font family

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,6 @@
   </div>
 </template>
 
-
 <script>
 import VNavbar from "./components/Navbar.vue";
 import VFooter from "./components/Footer.vue"
@@ -18,7 +17,10 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
 html {
   scroll-behavior: smooth;
+  font-family: 'Poppins', sans-serif;
 }
 </style>
